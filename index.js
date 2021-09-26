9ffc49968c28d021b65791c1cb7f61042b52b734
UshaGururaj <usha.neogrammer@gmail.com>
2021-09-26 14:58:40 +0530

    fix : added validation
    Added negative lucky  number validation.

--- index.js
+++ index.js
@@ -14,18 +14,25 @@ function checkBirthDateIsLucky(){
     var dob = Array.from(dateOfBirth.value);
     let sum=0;
     if(dateOfBirth.value&&luckyNumber.value){
+
+        if(Number(luckyNumber.value) <= 0){
+
+            isLucky.innerText="Lucky number should be greater than 0"; 
+            
+        } else {
         //20210825
-        for(let index=0;index<dateOfBirth.value.length;index++){
-            if(dob[index]==="-"){
-            }else{
-                birthDaystring +=dob[index];
-                sum = Number(sum)+Number(dob[index]);//0+2,2+0,2+2
+            for(let index=0;index<dateOfBirth.value.length;index++){
+                if(dob[index]==="-"){
+                }else{
+                    birthDaystring +=dob[index];
+                    sum = Number(sum)+Number(dob[index]);//0+2,2+0,2+2
+                }
             }
-        }
-        if((sum%luckyNumber.value) === 0){
-            isLucky.innerText="Your birthday is lucky..🥳";
-        }else{
-            isLucky.innerText="Your birthday is not that lucky..🤥";
+            if((sum%luckyNumber.value) === 0){
+                isLucky.innerText="Your birthday is lucky..🥳";
+            }else{
+                isLucky.innerText="Your birthday is not that lucky..🤥";
+            }
         }
     }else{
         isLucky.innerText="Please enter both Date of birth and Lucky number."
